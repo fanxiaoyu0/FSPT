a84bc0a7a2850adc03f30866d9277f53da789957
fanxiaoyu <fanxiaoyu1234@gmail.com>
2022-05-10 16:46:21 +0800

    Pretraining ConvMixer

--- src/main.py
+++ src/main.py
@@ -32,21 +32,20 @@ writer = SummaryWriter()
 jt.flags.use_cuda=1
 jt.set_global_seed(648)
 
-def send_email_to_myself():
-    # Install Courier SDK: pip install trycourier
+def send_email_to_myself(stringData):
     client = Courier(auth_token="pk_prod_FRV8652N0ZMXW1KPF236PV7ADNV1")
     resp = client.send_message(
         message={
             "to": {
-            "email": "fan-xy19@mails.tsinghua.edu.cn"
+                "email": "fan-xy19@mails.tsinghua.edu.cn"
             },
             "content": {
-            "title": "Welcome to Courier!",
-            "body": "Want to hear a joke? {{joke}}"
+                "title": stringData,
+                "body": stringData
             },
-            "data":{
-            "joke": "Why does Python live on land? Because it is above C level"
-            }
+            # "data":{
+            #     "joke": "Why does Python live on land? Because it is above C level"
+            # }
         }
     )
 
@@ -148,7 +147,7 @@ def pretrain_one_epoch(model:nn.Module, train_loader, criterion, optimizer, accu
     model.train()
     total_acc = 0
     total_num = 0
-    losses = []
+    lossList = []
     for i, (images, labels) in enumerate(train_loader):
         images,labels=cutmix(images,labels,num_classes=1020)
         output = model(images)
@@ -160,73 +159,62 @@ def pretrain_one_epoch(model:nn.Module, train_loader, criterion, optimizer, accu
         acc = np.sum(pred == np.argmax(labels.data, axis=1))
         total_acc += acc
         total_num += labels.shape[0]
-        losses.append(loss.data[0])
+        lossList.append(loss.data[0])
     scheduler.step()
-    return round(total_acc/total_num,4),round(sum(losses)/len(losses),4)
+    return round(total_acc/total_num,4),round(sum(lossList)/len(lossList),4)
 
-def train_one_epoch(model:nn.Module, train_loader, criterion, optimizer, accum_iter, scheduler):
+def train_one_epoch(model:nn.Module, trainDataSet, criterion, optimizer, scheduler):
     model.train()
-    total_acc = 0
-    total_num = 0
-    losses = []
-    for i, (images, labels) in enumerate(train_loader):
-        # images,labels=cutmix(images,labels,num_classes=1020)
+    rightCount = 0
+    totalCount = 0
+    lossList = []
+    for index, (images, labels) in enumerate(trainDataSet):
+    # for index, (images, labels) in tqdm(enumerate(trainDataSet)):
         output = model(images)
         loss = criterion(output, labels)
         optimizer.backward(loss)
-        if (i + 1) % accum_iter == 0 or i + 1 == len(train_loader):
-            optimizer.step(loss)
-        pred = np.argmax(output.data, axis=1)
-        acc = np.sum(pred == labels.data)
-        # acc = np.sum(pred == np.argmax(labels.data, axis=1))
-        total_acc += acc
-        total_num += labels.shape[0]
-        losses.append(loss.data[0])
-        if i%100==0:
-            print(i,loss.data[0])
+        optimizer.step(loss)
+        predictLabel = np.argmax(output.data, axis=1)
+        rightCount += np.sum(predictLabel == labels.data)
+        totalCount += labels.shape[0]
+        lossList.append(loss.data[0])
+        if index % 200 == 0:
+            print("index:", index, "loss:", loss.data[0])
     scheduler.step()
-    return round(total_acc/total_num,4),round(sum(losses)/len(losses),4)
+    return round(rightCount/totalCount,4),round(sum(lossList)/len(lossList),4)
 
-def valid_one_epoch(model:nn.Module, val_loader):
+def validate_one_epoch(model:nn.Module, validateDataSet,criterion):
     model.eval()
-    total_acc = 0
-    total_num = 0
-    for i, (images, labels) in enumerate(val_loader):
+    rightCount = 0
+    totalCount = 0
+    lossList = []
+    for index, (images, labels) in enumerate(validateDataSet):
         output = model(images)
-        pred = np.argmax(output.data, axis=1)
-        acc = np.sum(pred == labels.data)
-        total_acc += acc
-        total_num += labels.shape[0]
-    acc = total_acc / total_num
-    return round(acc,4)
+        loss = criterion(output, labels)
+        predictLabel = np.argmax(output.data, axis=1)
+        rightCount += np.sum(predictLabel == labels.data)
+        totalCount += labels.shape[0]
+        lossList.append(loss.data[0])
+    return round(rightCount/totalCount,4),round(sum(lossList)/len(lossList),4)
 
-def calculate_test_set_accuracy(model:nn.Module, testLoader):
+def get_test_data_set_accuracy(model,imageSize,batchSize):
+    trainDataSet, validateDataSet, testDataSet=construct_data_loader(imageSize=imageSize,batchSize=batchSize)
     model.eval()
-
-    total_acc = 0
-    total_num = 0
-    # pbar = tqdm(testLoader, desc="calculate_test_set_accuracy")
-    for index,(images, labels) in enumerate(testLoader):        
+    rightCount = 0
+    totalCount = 0
+    for index, (images, labels) in enumerate(testDataSet):
         output = model(images)
-        pred = np.argmax(output.data, axis=1)
-        acc = np.sum(pred == labels.data)
-        total_acc += acc
-        total_num += labels.shape[0]
-        # pbar.set_description(f'acc={total_acc / total_num:.4f}')
-        # if index%200==0:
-            # print(index,"test accuracy",round(total_acc/total_num,4))
-    acc = total_acc / total_num
-    return round(acc,4)
+        predictLabel = np.argmax(output.data, axis=1)
+        rightCount += np.sum(predictLabel == labels.data)
+        totalCount += labels.shape[0]
+    return round(rightCount/totalCount,4)
 
-def train(model:nn.Module,modelName,learningRate,epochs,etaMin,imageSize,savedName):
-    startTime=time.time()
-    # region Processing data 
-    # resizedImageSize=imageSize
-    # croppedImagesize=imageSize-32
+def construct_data_loader(imageSize,batchSize):
+    resizedImageSize=imageSize+32
     data_transforms = {
         'train': transform.Compose([
-            transform.Resize((imageSize,imageSize)),
-            # transform.RandomCrop((croppedImagesize, croppedImagesize)),       # 从中心开始裁剪
+            transform.Resize((resizedImageSize,resizedImageSize)),
+            transform.RandomCrop((imageSize, imageSize)),       # 从中心开始裁剪
             transform.RandomHorizontalFlip(p=0.5),  # 随机水平翻转 选择一个概率
             transform.RandomVerticalFlip(p=0.5),    # 随机垂直翻转
             transform.RandomRotation(90),
@@ -234,7 +222,7 @@ def train(model:nn.Module,modelName,learningRate,epochs,etaMin,imageSize,savedNa
             transform.ImageNormalize([0.485, 0.456, 0.406],
                                     [0.229, 0.224, 0.225])  # 均值，标准差
         ]),
-        'valid': transform.Compose([
+        'validate': transform.Compose([
             transform.Resize((imageSize, imageSize)),
             transform.ToTensor(),
             transform.ImageNormalize([0.485, 0.456, 0.406],
@@ -248,45 +236,39 @@ def train(model:nn.Module,modelName,learningRate,epochs,etaMin,imageSize,savedNa
         ])
     }
     print("Loading data...")
-    batch_size = 64
     data_dir = '../data'
-    image_datasets = {x: jt.dataset.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in
-                    ['train', 'valid', 'test']}
-    traindataset = image_datasets['train'].set_attrs(batch_size=batch_size, shuffle=True)
-    validdataset = image_datasets['valid'].set_attrs(batch_size=batch_size, shuffle=False)
-    testDataSet = image_datasets['test'].set_attrs(batch_size=batch_size, shuffle=False)
-    # endregion
+    image_datasets = {x: jt.dataset.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'validate', 'test']}
+    trainDataSet = image_datasets['train'].set_attrs(batch_size=batchSize, shuffle=True)
+    validateDataSet = image_datasets['validate'].set_attrs(batch_size=batchSize, shuffle=False)
+    testDataSet = image_datasets['test'].set_attrs(batch_size=batchSize, shuffle=False)
+    return trainDataSet, validateDataSet, testDataSet
 
-    # region model and optimizer
+def train(model:nn.Module,modelName,learningRate,epochs,etaMin,imageSize,batchSize,savedName):
+    trainDataSet, validateDataSet, testDataSet=construct_data_loader(imageSize,batchSize) 
+
     # criterion = SoftLabelCrossEntropyLoss()
     criterion = nn.CrossEntropyLoss()
     optimizer = nn.Adam(model.parameters(), lr=learningRate, weight_decay=1e-3)
     scheduler = CosineAnnealingLR(optimizer, 15, eta_min=etaMin)
-    # endregion
 
-    # region train and valid
     print("----------------- A new trial ---------------------")
-    print("modelName",modelName,"learning rate:",learningRate,"etaMin",etaMin,"imgSize",imageSize,"savedName",savedName,\
+    print("modelName",modelName,"learning rate:",learningRate,"etaMin",etaMin,"imgSize",imageSize,"batchSize",batchSize,"savedName",savedName,\
         "criterion","CrossEntropyLoss","weight_decay",1e-3,"TMax",15)
     with open("../result/summary/"+modelName+".txt","a") as f:
         f.write("modelName: "+modelName+"  learning rate:"+str(learningRate)+"  etaMin:"+str(etaMin)+"  imgSize:"+str(imageSize)+\
-            "  savedName:"+savedName+"  criterion:"+"  CrossEntropyLoss"+"  weight_decay:"+str(1e-3)+"  TMax:"+str(15)+"\n")
+            "  batchSize:"+str(batchSize)+"  savedName:"+savedName+"  criterion:"+"  CrossEntropyLoss"+"  weight_decay:"+str(1e-3)+"  TMax:"+str(15)+"\n")
     maxBearableEpochs=50
     noProgressEpochs=0
     stopEpoch=0
     currentBestAccuracy=0.0
     currentBestEpoch=0
-    for epoch in range(epochs):
-        trainAccuracy,trainLoss=train_one_epoch(model, traindataset, criterion, optimizer, 1, scheduler)
-        endTime=time.time()
-        print("Time after train one epoch:",endTime-startTime)
-        validAccuracy=valid_one_epoch(model, validdataset)
-        endTime=time.time()
-        print("Time after valid one epoch:",endTime-startTime)
-        print("epoch:",epoch,"validAccuracy:",validAccuracy,"trainAccuracy:",trainAccuracy,"delta:",round(validAccuracy-currentBestAccuracy,4))
-        writer.add_scalars(modelName,{'trainAccuracy':trainAccuracy,'validAccuracy':validAccuracy,"trainLoss":trainLoss}, epoch)
-        if validAccuracy > currentBestAccuracy:
-            currentBestAccuracy=validAccuracy
+    for epoch in tqdm(range(epochs),desc="Training"):
+        trainAccuracy,trainLoss=train_one_epoch(model, trainDataSet, criterion, optimizer, scheduler)
+        validateAccuracy,validateLoss=validate_one_epoch(model, validateDataSet,criterion)
+        print("epoch:",epoch,"validateAccuracy:",validateAccuracy,"trainAccuracy:",trainAccuracy,"delta:",round(validateAccuracy-currentBestAccuracy,4))
+        writer.add_scalars(modelName,{'trainAccuracy':trainAccuracy,'validateAccuracy':validateAccuracy,"trainLoss":trainLoss,"validateLoss":validateLoss}, epoch)
+        if validateAccuracy > currentBestAccuracy:
+            currentBestAccuracy=validateAccuracy
             currentBestEpoch=epoch
             model.save("../weight/"+modelName+"/"+savedName)
             noProgressEpochs=0
@@ -296,95 +278,24 @@ def train(model:nn.Module,modelName,learningRate,epochs,etaMin,imageSize,savedNa
                 stopEpoch=epoch
                 break
         stopEpoch=epoch
-    testAccuracy=valid_one_epoch(model,testDataSet)
-    endTime=time.time()
-    print("Time used:",endTime-startTime)
+    testAccuracy=get_test_data_set_accuracy(model,imageSize,batchSize)
     print("==========================================================================================")
-    print("testAccuracy",testAccuracy,"validAccuracy",currentBestAccuracy,"bestEpoch",currentBestEpoch,"stopEpoch",stopEpoch)
+    print("testAccuracy",testAccuracy,"validateAccuracy",currentBestAccuracy,"bestEpoch",currentBestEpoch,"stopEpoch",stopEpoch)
     print("==========================================================================================")
-    send_email_to_myself()
-    # endregion
-
-def pretrain(model:nn.Module,modelName,learningRate,epochs,etaMin,savedName):
-    # region Processing data 
-    resizedImageSize = 256
-    croppedImagesize=resizedImageSize-32
-    data_transforms = {
-        'pretrain': transform.Compose([
-            transform.Resize((resizedImageSize,resizedImageSize)),
-            transform.RandomCrop((croppedImagesize, croppedImagesize)),       # 从中心开始裁剪
-            transform.RandomHorizontalFlip(p=0.5),  # 随机水平翻转 选择一个概率
-            transform.RandomVerticalFlip(p=0.5),    # 随机垂直翻转
-            transform.RandomRotation(90),
-            transform.ToTensor(),
-            transform.ImageNormalize([0.485, 0.456, 0.406],
-                                    [0.229, 0.224, 0.225])  # 均值，标准差
-        ]),
-        'valid': transform.Compose([
-            transform.Resize((croppedImagesize, croppedImagesize)),
-            transform.ToTensor(),
-            transform.ImageNormalize([0.485, 0.456, 0.406],
-                                    [0.229, 0.224, 0.225])
-        ]),
-        'test': transform.Compose([
-            transform.Resize((croppedImagesize, croppedImagesize)),
-            transform.ToTensor(),
-            transform.ImageNormalize([0.485, 0.456, 0.406],
-                                    [0.229, 0.224, 0.225])
-        ])
-    }
-    batch_size = 64
-    data_dir = '../data'
-    image_datasets = {x: jt.dataset.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in
-                    ['pretrain', 'valid', 'test']}
-    traindataset = image_datasets['pretrain'].set_attrs(batch_size=batch_size, shuffle=True)
-    # endregion
-    
-    # region hyper parameters
-    criterion = SoftLabelCrossEntropyLoss()
-    optimizer = nn.Adam(model.parameters(), lr=learningRate, weight_decay=1e-3)    
-    scheduler = CosineAnnealingLR(optimizer, 15, eta_min=etaMin)
-    # endregion
-
-    # region pretrain
-    print("----------------- A new trial ---------------------")
-    print("modelName",modelName,"learning rate:",learningRate,"epochs",epochs,"etaMin",etaMin,"savedName",savedName)
-    currentBestAccuracy=0.0
-    for epoch in range(epochs):
-        trainAccuracy,trainLoss=pretrain_one_epoch(model, traindataset, criterion, optimizer, 1, scheduler)
-        print("epoch:",epoch,"trainAccuracy:",trainAccuracy,"delta:",round(trainAccuracy-currentBestAccuracy,4))
-        writer.add_scalars(modelName,{'trainAccuracy':trainAccuracy,"trainLoss":trainLoss}, epoch)
-        if trainAccuracy > currentBestAccuracy:
-            currentBestAccuracy=trainAccuracy
-        if epoch%20==0:    
-            model.save("../model/"+modelName+"/"+savedName)
-    # endregion
-
+    send_email_to_myself(modelName+" Training Completed!")
 
 if __name__=="__main__":
-    # send_email_to_myself()
-    # vitModel = VisionTransformer(img_size=224,patch_size=16, embed_dim=768, depth=8, num_heads=8, mlp_ratio=3.,num_classes=5001)
-    # train(model=vitModel,modelName="ViT",learningRate=5e-5,epochs=300,etaMin=1e-5,imageSize=224,savedName="1.pkl",)
+    # vitModel = VisionTransformer(img_size=224,patch_size=16, embed_dim=768, depth=8, num_heads=8, mlp_ratio=3.,num_classes=102)
+    # train(model=vitModel,modelName="ViT",learningRate=5e-5,epochs=200,etaMin=1e-5,imageSize=224,batchSize=64,savedName="2.pkl",)
 
-    mlpMixerModel=MLPMixerForImageClassification(
-        in_channels=3,patch_size=16, d_model=512, depth=12, num_classes=5001,image_size=224,dropout=0)
-    train(model=mlpMixerModel,modelName="MLPMixer",learningRate=2.3e-5,epochs=200,etaMin=1e-5,imageSize=224,savedName="1.pkl")
-    # print(sys.argv)
-    # if sys.argv[1]=="ViT":
-    #     print("ViT")
-    #     vitModel = VisionTransformer(img_size=224,patch_size=16, embed_dim=768, depth=8, num_heads=8, mlp_ratio=3.,num_classes=5001)
-    #     train(model=vitModel,modelName="ViT",learningRate=5e-5,epochs=300,etaMin=1e-5,imageSize=224,savedName=sys.argv[2]+".pkl",)
-    # elif sys.argv[1]=="MLPMixer":
-    #     mlpMixerModel=MLPMixerForImageClassification(
-    #         in_channels=3,patch_size=16, d_model=512, depth=12, num_classes=5001,image_size=224,dropout=0)
-    #     # print(mlpMixerModel)
-    #     # print(mlpMixerModel._modules.keys())
-    #     train(model=mlpMixerModel,modelName="MLPMixer",learningRate=2.3e-5,epochs=200,etaMin=1e-5,imageSize=224,savedName=sys.argv[2]+".pkl")
-    # elif sys.argv[1]=="ConvMixer":
-    #     convMixerModel=ConvMixer(dim = 768, depth = 32, kernel_size=7, patch_size=7,n_classes=5001)
-    #     train(model=convMixerModel,modelName="ConvMixer",learningRate=1e-4,epochs=500,etaMin=1e-7,imageSize=224,savedName=sys.argv[2]+".pkl")
-    # elif sys.argv[1]=="ResNet":
-    #     resnetModel=Resnet50(num_classes=5001)
-    #     train(model=resnetModel,modelName="ResNet",learningRate=1e-4,epochs=1600,etaMin=1e-7,imageSize=224,savedName=sys.argv[2]+".pkl")
+    # mlpMixerModel=MLPMixerForImageClassification(
+    #     in_channels=3,patch_size=16, d_model=512, depth=12, num_classes=102,image_size=224,dropout=0)
+    # train(model=mlpMixerModel,modelName="MLPMixer",learningRate=2.3e-5,epochs=200,etaMin=1e-5,imageSize=224,batchSize=64,savedName="1.pkl")
+    
+    convMixerModel=ConvMixer(dim = 768, depth = 32, kernel_size=7, patch_size=7,n_classes=5001)
+    train(model=convMixerModel,modelName="ConvMixer",learningRate=1e-4,epochs=500,etaMin=1e-7,imageSize=224,batchSize=32,savedName="1.pkl")
+    
+    # resnetModel=Resnet50(num_classes=5001)
+    # train(model=resnetModel,modelName="ResNet",learningRate=1e-4,epochs=1600,etaMin=1e-7,imageSize=224,batchSize=64,savedName="1.pkl")
     
     writer.close()
